7065a515db63144d173cc5699032d8b247fba72c
Arpit Mishra <arpitm.p24@medhaviskillsuniversity.edu.in>
2024-12-11 21:16:37 +0530

    CSS of main heading

---
+++ script.js
@@ -0,0 +1,11 @@
+.header{
+    display: flex;
+}
+h1{
+    align-items: center;
+    color:teal;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    font-size: 3rem;
+    margin-left: 500px;
+    margin-top: 95px;
+}
